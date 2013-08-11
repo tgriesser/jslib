7fc681af8373c2d21117b67bb829e3716476fb45
Tim Griesser <tgriesser10@gmail.com>
2013-08-10 20:04:19 -0400

    cleanup the amd boilerplate

--- jslib.js
+++ jslib.js
@@ -2,7 +2,22 @@
 //     http://tgriesser.com/jslib
 //     (c) 2013 Tim Griesser
 //     Jslib may be freely distributed under the MIT license.
-(function(JslibFactory) {
+(function(define) { "use strict";
+
+define(function(_) {
+
+  var Jslib = function() {};
+
+  Jslib.VERSION = '0.0.0';
+
+  Jslib.prototype = {};
+
+  return Jslib;
+
+});
+
+// Boilerplate definition block... get the correct dependencies and initialize everything.
+})(function(JslibFactory) {
 
   if (typeof exports === 'object') {
     module.exports = JslibFactory(require('underscore'));
@@ -20,16 +35,4 @@
     };
   }
 
-}).call(this, (function(_) {
-
-  "use strict";
-
-  var Jslib = function() {};
-
-  Jslib.VERSION = '0.0.0';
-
-  Jslib.prototype = {};
-
-  return Jslib;
-
-}));
+});
